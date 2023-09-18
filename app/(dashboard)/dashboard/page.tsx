2375488aa7575efb394a96ca6d7803907ce3977f
Erickb <wr.t8r1@gmai.com>
2023-09-18 11:50:37 -0700

    add recent changes on Sidebar

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import { options } from '@/app/api/auth/[...nextauth]/options'
 import { getServerSession } from 'next-auth/next'
@@ -5,7 +6,11 @@ import { redirect } from 'next/navigation'
 
 export default async function Dashboard() {
   const session = await getServerSession(options)
+  { console.log(session?.user?.image) }
 
+  const imageLoader = (session: any) => {
+    return `session.user.image`
+  }
 
   if (!session) {
     console.log(JSON.stringify(session))
@@ -15,6 +20,8 @@ export default async function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex text-black">Dashboard: {session?.user?.name}</div>
+
+      <Image loader={imageLoader} src={`session?.user?.image`} width={400} height={400} alt="Picture of the author" />
     </main>
   )
 }
