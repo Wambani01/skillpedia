99af2a68a374686636013916563d98ecd93b6754
Erickb <wr.t8r1@gmai.com>
2023-09-19 02:02:24 -0700

    Add shadcn components to project

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import Image from 'next/image'
 import { options } from '@/app/api/auth/[...nextauth]/options'
 import { getServerSession } from 'next-auth/next'
@@ -8,20 +8,14 @@ export default async function Dashboard() {
   const session = await getServerSession(options)
   { console.log(session?.user?.image) }
 
-  const imageLoader = (session: any) => {
-    return `session.user.image`
-  }
 
   if (!session) {
-    console.log(JSON.stringify(session))
     redirect('/api/auth/signin?callbackUrl=/dashboard')
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex text-black">Dashboard: {session?.user?.name}</div>
-
-      <Image loader={imageLoader} src={`session?.user?.image`} width={400} height={400} alt="Picture of the author" />
     </main>
   )
 }
