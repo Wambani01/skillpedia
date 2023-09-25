873e4f947da374ba396b04265fe87074e1749c5c
Immanuel Kituku <ikituku@nbihosp.org>
2023-09-25 08:10:57 +0300

    Add form for finding peers

--- app/(dashboard)/peerzone/page.tsx
+++ app/(dashboard)/peerzone/page.tsx
@@ -1,9 +1,76 @@
 "use client";
 
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  categories: z.string(),
+  searchPeer: z.string(),
+});
 
 const PeerZone = () => {
-  return <div>PeerZone</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      categories: "",
+      searchPeer: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <div className="m-5">
+      <h1 className="text-xl">Find a Peer</h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="categories"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Select Interested Categories"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="searchPeer"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Search" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default PeerZone;
